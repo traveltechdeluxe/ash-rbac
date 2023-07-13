83d0e3b6766295ac225c1588d20dd638cc1477e2
Barnabas Jovanovics <barnabas.jovanovics@gmail.com>
2023-07-13 16:10:45 +0200

    feat: prepare release 0.0.1 (#2)

--- mix.exs
+++ mix.exs
@@ -4,11 +4,26 @@ defmodule AshRbac.MixProject do
   def project do
     [
       app: :ash_rbac,
-      version: "0.1.0",
+      version: "0.0.1",
+      description: "A small extension for easier application of policies",
+      package: [
+        maintainers: ["travel tech d.luxe GmbH"],
+        licenses: ["MIT"],
+        links: %{
+          "GitHub" => "https://github.com/traveltechdeluxe/ash-rbac",
+          "Travel Tech D.Luxe" => "https://www.traveltechdeluxe.com/"
+        }
+      ],
       elixir: "~> 1.14",
       elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      docs: [
+        # The main page in the docs
+        main: "AshRbac",
+        logo: "logo.png",
+        extras: ["README.md"]
+      ]
     ]
   end
 
@@ -25,10 +40,10 @@ defmodule AshRbac.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      {:ash, branch: "main", github: "ash-project/ash", override: true},
-      {:spark, branch: "main", github: "ash-project/spark", override: true},
+      {:ash, "~> 2.11.8"},
       {:credo, "~> 1.6", only: [:dev, :test], runtime: false},
       {:elixir_uuid, "~> 1.2", only: [:test], runtime: false},
+      {:ex_doc, "~> 0.27", only: :dev, runtime: false},
       {:git_hooks, "~> 0.7.3", only: :dev},
       # test watcher
       {:mix_test_watch, "~> 1.0", only: :dev, runtime: false}
