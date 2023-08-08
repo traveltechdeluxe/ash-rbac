66e85f097abe4a04b2c975c76f812a522aaca61a
Barnabas Jovanovics <barnabas.jovanovics@gmail.com>
2023-08-08 15:25:13 +0200

    prepare release 0.3.2

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule AshRbac.MixProject do
   def project do
     [
       app: :ash_rbac,
-      version: "0.3.1",
+      version: "0.3.2",
       description: "A small extension for easier application of policies",
       package: [
         maintainers: ["travel tech d.luxe GmbH"],
