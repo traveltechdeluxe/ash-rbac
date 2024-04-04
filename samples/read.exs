2aefef914c326b3b8b325a402936708369b01156
Barnabas Jovanovics <barnabas.jovanovics@gmail.com>
2024-04-04 15:54:39 +0200

    update benchmarks

--- samples/read.exs
+++ samples/read.exs
@@ -1,15 +1,15 @@
-alias AshRbacTest.{Api, ChildResource, RootResource, SharedResource}
+alias AshRbacTest.{ChildResource, RootResource, SharedResource}
 
 
-root_resource = Api.create!(Ash.Changeset.for_create(RootResource, :create))
-Api.create!(Ash.Changeset.for_create(SharedResource, :create))
-      Api.create!(Ash.Changeset.for_create(ChildResource, :create, %{root_id: root_resource.id}))
+root_resource = Ash.create!(Ash.Changeset.for_create(RootResource, :create), authorize?: false)
+Ash.create!(Ash.Changeset.for_create(SharedResource, :create), authorize?: false)
+Ash.create!(Ash.Changeset.for_create(ChildResource, :create, %{root_id: root_resource.id}), authorize?: false)
 
 Benchee.run(%{
   "read" => fn ->
              RootResource
              |> Ash.Query.select([:id])
              |> Ash.Query.load([:child, :number, :children])
-             |> Api.read(actor: %{roles: [:user]})
+             |> Ash.read(actor: %{roles: [:user]})
   end
 }, profile_after: true)
